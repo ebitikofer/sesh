408463d34d2d8ae72ec8c887635ee3d8bb6c20ee
Eric Bitikofer <e.bitikofer@gmail.com>
2017-11-09 17:59:06 -0500

    added some unmerged stuff from old non-git copy, needs looking through

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sesh
+
+This is a library I made for some added usefulness to Scheme
+
+2016.eric.bitikofer
--- f.scm
+++ f.scm
@@ -3,12 +3,11 @@
 ;CS 3200 - HW 5
 ;Scheme functions to do each of the following
 ; 1. extract-lists - extracts all list elements from a list
-; 2. extract-lists - extracts all list elements from a list
-; 3. extract-lists - extracts all list elements from a list
-; 4. extract-lists - extracts all list elements from a list
-; 5. extract-lists - extracts all list elements from a list
-; 6. extract-lists - extracts all list elements from a list
-;Write comments describing each function.
+; 2. count-evens - a function that counts all even numbers inside a list.
+; 3. create-pairs - a function that creates a list of pairs from its two list parameters.
+; 4. product - a function that takes two lists as parameters and returns all possible pairs of the of the two sets.
+; 5. remove-duplicates - a function that removes all duplicates from a list
+; 6. fibonacci - a more efficient Fibonacci numbers function than the one discussed in class.
 
 ;extract-lists - a function that extracts all list elements from a list
 (define (extract-lists lst) ;define
@@ -60,12 +59,13 @@
 
 ;remove-duplicates - a function that removes all duplicates from a list
 ;(define (remove-duplicates lst) ;define
-  ;(cond ;condition check
-    ;((null? lst) '()) ;if the list is null print null
-    ;((not (member? (car lst) (cdr lst))) (cons (car lst) (remove-duplicates (cdr lst)))) ;if the first element is not a member of the rest of the list print it and move to the rest of the list
-    ;(else (remove-duplicates (cdr lst))) ;else move to the rest of the list
-  ;)
+;  (cond ;condition check
+;    ((null? lst) '()) ;if the list is null print null
+;    ((not (member? (car lst) (cdr lst))) (cons (car lst) (remove-duplicates (cdr lst)))) ;if the first element is not a member of the rest of the list print it and move to the rest of the list
+;    (else (remove-duplicates (cdr lst))) ;else move to the rest of the list
+;  )
 ;)
+
 
 ;(define (member el lst)
 ;  (memberator el lst #f)
@@ -78,22 +78,36 @@
 ;  )
 ;)
 
+;(define (member? el lst) ;define
+;  (cond ;condition check
+;    ((null? lst) #f) ;if the list is null return false
+;    ((= el (car lst)) #t) ;if the member is equal first element int the list return true
+;    ((member? el (cdr lst)) #t) ;if the element is a member rest of the list return true
+;    (else #f) ;else return false
+;  )
+;)
+
+;remove-duplicates - a function that removes all duplicates from a list
+(define (remove-duplicates lst) ;define
+    (delete-duplicates lst)
+)
+
 ;fibonacci - a more efficient Fibonacci numbers function than the one discussed in class.
 (define (fibonacci n) ;define
   (fibber 1 0 n) ;initial call
 )
 
 (define (fibber a b cntr) ;define helper
-  (if (= cntr 0) b (fibber (+ a b) a (- cntr 1))) ;if counter is zero b else move to next value in sequence
+  (if (= cntr 0) b (fibber (+ a b) a (- cntr 1))) ;if counter is zero b else move to next value in sequence adding a and b as new a, a as new b, and decrementing the counter
 )
 
-  ;(load "f.scm")
+;(load "f.scm")
 
-  ;Example:
-    ;(product '(1 2) '(a b))
-    ;returns ((1 a) (1 b) (2 a) (2 b))
-  ;Examples:
-    ;(remove-duplicates '(1 2 2 1 1 1 3 3 3 4 2 5))
-    ;returns (1 2 3 4 5)
-    ;(remove-duplicates '(h h h e h h l l l h o e e o o))
-    ;returns (h e l o)
+;Example:
+;(product '(1 2) '(a b))
+;returns ((1 a) (1 b) (2 a) (2 b))
+;Examples:
+;(remove-duplicates '(1 2 2 1 1 1 3 3 3 4 2 5))
+;returns (1 2 3 4 5)
+;(remove-duplicates '(h h h e h h l l l h o e e o o))
+;returns (h e l o)
